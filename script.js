48742ae3616163a09f0a5868c0ecba96ffea5953
Hestaine <ariffhestaine@gmail.com>
2024-02-06 22:46:36 +0100

    giek

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+const menuBtn = document.querySelector(".menu");
+const closeBtn = document.querySelector(".close");
+const nav = document.querySelector("nav");
+const container = document.querySelector(".container");
+
+menuBtn.addEventListener("click", (e) => {
+  nav.classList.remove("hidden");
+  container.style.filter : blur(5px);
+});
