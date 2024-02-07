c6367fa0f756a431a0df569c15b86703c17af184
Hestaine <ariffhestaine@gmail.com>
2024-02-07 10:10:12 +0100

    jhl

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const menuBtn = document.querySelector(".menu");
 const closeBtn = document.querySelector(".close");
 const nav = document.querySelector("nav");
 const container = document.querySelector(".container");
+const overlay = document.querySelector(".overlay");
 
 menuBtn.addEventListener("click", (e) => {
   nav.classList.remove("hidden");
-  container.classList.add("blurred");
+  overlay.classList.add("dark-overlay");
+});
+
+closeBtn.addEventListener("click", (e) => {
+  nav.classList.add("hidden");
+  overlay.classList.remove("dark-overlay");
 });
